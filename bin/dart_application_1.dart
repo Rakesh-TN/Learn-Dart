546256e2b811997caae287c3338efb8bfeddbee9
Rakesh-TN <tnrakesh68@outlook.com>
2023-11-24 01:05:12 +0530

    Day : 1 Dart Variables

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,30 @@
+void main() {
+  print("Learning Dart in Codemy.com YT channel");
+  
+  // New Method 
+  print("10 * 10 = ${10 * 10}");
+
+  // Variables
+
+  // Strings
+  // var fname = 'Rocky';
+  String firname = "Rakesh TN";
+
+  // Integer
+  // var fage = 22;
+  int firage = 22;
+
+  // Dynamic
+  dynamic fage = "BCom";
+
+  // Const and Final
+
+  const String userName = "Rakesh";
+  final String nickName = "Rocky";
+
+  print(firname);
+  print(fage);
+  print(firage);
+  print(userName);
+  print(nickName);
+}
